2b32741189c9c554b38ca0714e4749cf9ce314f2
johnathanwastaken <danielortiz2001@hotmail.com>
2022-11-05 13:46:47 -0700

    Navbar with style

--- views/default.jsx
+++ views/default.jsx
@@ -1,18 +1,42 @@
-const React = require('react')
+const React = require("react");
 
-function Def (html) {
-    return (
-        <html>
-            <head>
-                <title>Title</title>
-                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossOrigin="anonymous"/>
-                <link rel="stylesheet" href="/css/style.css" /> 
-            </head>
-            <body>
-                {html.children}
-            </body>
-        </html>
-    )
+function Def(html) {
+  return (
+    <html>
+      <head>
+        <title>Title</title>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+          crossOrigin="anonymous"
+        />
+        <link rel="stylesheet" href="/css/style.css" />
+      </head>
+      <body>
+        <nav>
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/places">Places</a>
+            </li>
+            <li>
+              <a href="/places/new">Add Place</a>
+            </li>
+          </ul>
+        </nav>
+        {html.children}
+        <footer>
+          Designed and Programmed by
+          <a href="https://www.linkedin.com/in/johnathan-ortiz-flores-a9545823b/">
+            Johnathan Ortiz-Flores
+          </a>
+        </footer>
+      </body>
+    </html>
+  );
 }
 
-module.exports = Def
+module.exports = Def;
